30add9a9a5be106db86a7b1c85f4d1faf30e05c1
Rob Martorano (netid: rfm21) <rfm21@duke.edu>
2015-10-06 13:09:15 -0400

    howdy

--- gitshit.php
+++ gitshit.php
@@ -1,1 +1,13 @@
-<?php `git pull`;
+<?php
+$gh_ips = array('171.149.246.236', '95.92.138.4');
+if (in_array($_SERVER['REMOTE_ADDR'], $gh_ips) === false) {
+    header('Status: 403 Your IP is not on our list; bugger off', true, 403);
+    mail('root', 'Unfuddle hook error: bad ip', $_SERVER['REMOTE_ADDR']);
+    die(1);
+}
+$BRANCH = $_GET['branch'];
+if (!empty($BRANCH)) {
+    $output = shell_exec("cd /home/home1/robm/public_html/tester/; git pull origin {$BRANCH};");
+    echo "<pre>$output</pre>";
+}
+die("done " . mktime()); ?>Hi
